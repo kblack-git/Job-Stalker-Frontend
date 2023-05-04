36dc9d8336c2f8319396778fbe8df2a791592e19
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-04 14:00:42 -0400

    added scripts

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 
 
+let messageBox=document.querySelector('.tasks-wrapper');
+
+let messageJSON=localStorage.getItem('messageItem');
+let messageItem=JSON.parse(messageJSON);
+
+messageItem?null:messageItem=[];
+    
+messageItem.forEach(element => {
+    messageBox.innerHTML+=`
+    <div class="task">
+            <input class="task-item" name="task" type="checkbox" id="item-1" checked />
+            <label for="item-1">
+                <span class="label-text">blah blah blah</span>
+            </label>
+        <span class="tag approved">Sent</span>
+    </div>`;
+});
