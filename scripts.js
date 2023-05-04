ee3982a48dc9c98a28e559107bb3a5d6146b51aa
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-04 14:22:57 -0400

    added link to scripts

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 
 
-let messageBox=document.querySelector('.tasks-wrapper');
+let messageBox=document.querySelector('.log-wrapper');
 
 let messageJSON=localStorage.getItem('messageItem');
 let messageItem=JSON.parse(messageJSON);
 
 messageItem?null:messageItem=[];
-    
+messageBox.innerHTML+=`
+<div class="task">
+        <input class="task-item" name="task" type="checkbox" id="item-1" checked />
+        <label for="item-1">
+            <span class="label-text">blah blah blah</span>
+        </label>
+    <span class="tag approved">Sent</span>
+</div>`;
 messageItem.forEach(element => {
     messageBox.innerHTML+=`
     <div class="task">
