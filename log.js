a4c766d1857970e5c8ba800cad45d9f180c1e790
Quaya Brown <technoquaya@gmail.com>
2023-05-04 11:24:09 -0400

    front

---
+++ log.js
@@ -0,0 +1,14 @@
+var d = document.getElementById("login");
+var n = document.getElementById("register");
+var b = document.getElementById("btn");
+
+function register (){
+    d.style.left = "-400px";
+    n.style.leftm= "50px";
+    b.style.left = "110px";
+}
+function login (){
+    d.style.left = "50px";
+    n.style.leftm= "450px";
+    b.style.left = "0";
+}
