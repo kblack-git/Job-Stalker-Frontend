de0db983bb9fb4e8d8726f587b2b783ea12e5c64
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-04 14:24:43 -0400

    removed test innerhtml

--- scripts.js
+++ scripts.js
@@ -6,14 +6,7 @@ let messageJSON=localStorage.getItem('messageItem');
 let messageItem=JSON.parse(messageJSON);
 
 messageItem?null:messageItem=[];
-messageBox.innerHTML+=`
-<div class="task">
-        <input class="task-item" name="task" type="checkbox" id="item-1" checked />
-        <label for="item-1">
-            <span class="label-text">blah blah blah</span>
-        </label>
-    <span class="tag approved">Sent</span>
-</div>`;
+
 messageItem.forEach(element => {
     messageBox.innerHTML+=`
     <div class="task">
