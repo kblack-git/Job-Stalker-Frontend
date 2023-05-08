9534087949188853f9faf435361490d0fc9926b6
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-08 14:12:27 -0400

    working on log

--- public/javascripts/log.js
+++ public/javascripts/log.js
@@ -2,6 +2,12 @@ var d = document.getElementById("login");
 var n = document.getElementById("register");
 var b = document.getElementById("btn");
 
+let userName=document.querySelector('.user-name');
+let passWord=document.querySelector('.pass-word')
+let submitButton=document.querySelector('.submit-btn');
+let registerButton=document.querySelector('.register-btn');
+
+
 function register (){
     d.style.left = "-400px";
     n.style.leftm= "50px";
@@ -12,3 +18,7 @@ function login (){
     n.style.leftm= "450px";
     b.style.left = "0";
 }
+
+submitButton.addEventListener('click',(e)=>{
+    
+})
